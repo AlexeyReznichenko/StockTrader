1bac2878052b9383a5ff688100cf1efe0b8e2fea
AlexeyReznichenko <77417710+AlexeyReznichenko@users.noreply.github.com>
2021-01-15 14:04:50 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="body">
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    'app-header': Header
+  }
+}
+</script>
+
+<style lang="scss">
+  .body {
+    margin: 3rem 6.5rem;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
